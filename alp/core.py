27c20bd9d9c0fa8df7569039986bb555e5d8be24
Benjamin Knight <ben@benknight.me>
2014-02-09 17:31:32 -0800

    Getting rid of unnecessary alp components

--- alp/core.py
+++ alp/core.py
@@ -19,17 +19,17 @@ def bundle():
     global gBundleID
 
     if gBundleID is not None:
-	return gBundleID
+        return gBundleID
 
     infoPath = os.path.abspath("./info.plist")
     if os.path.exists(infoPath):
-	info = plistlib.readPlist(infoPath)
-	try:
-	    gBundleID = info["bundleid"]
-	except KeyError:
-	    raise Exception("Bundle ID not defined or readable from info.plist.")
+        info = plistlib.readPlist(infoPath)
+        try:
+            gBundleID = info["bundleid"]
+        except KeyError:
+            raise Exception("Bundle ID not defined or readable from info.plist.")
     else:
-	raise Exception("info.plist missing.")
+        raise Exception("info.plist missing.")
 
     return gBundleID
 
@@ -38,7 +38,7 @@ def args():
     # With thanks to Github's nikipore for the pointer re. unicodedata.
     returnList = []
     for arg in sys.argv[1:]:
-	returnList.append(decode(arg))
+        returnList.append(decode(arg))
     return returnList
 
 
@@ -50,7 +50,7 @@ def local(join=None):
     localPath = os.path.abspath("./")
 
     if join:
-	localPath = os.path.join(localPath, join)
+        localPath = os.path.join(localPath, join)
 
     return localPath
 
@@ -60,10 +60,10 @@ def cache(join=None):
     vPath = os.path.expanduser(os.path.join("~/Library/Caches/com.runningwithcrayons.Alfred-2/Workflow Data/", bundleID))
 
     if not os.path.exists(vPath):
-	os.makedirs(vPath)
+        os.makedirs(vPath)
 
     if join:
-	vPath = os.path.join(vPath, join)
+        vPath = os.path.join(vPath, join)
 
     return vPath
 
@@ -73,10 +73,10 @@ def storage(join=None):
     nvPath = os.path.expanduser(os.path.join("~/Library/Application Support/Alfred 2/Workflow Data/", bundleID))
 
     if not os.path.exists(nvPath):
-	os.makedirs(nvPath)
+        os.makedirs(nvPath)
 
     if join:
-	nvPath = os.path.join(nvPath, join)
+        nvPath = os.path.join(nvPath, join)
 
     return nvPath
 
@@ -84,49 +84,49 @@ def storage(join=None):
 def readPlist(path):
     # With thanks to https://github.com/congalong for solving the biplist problem
     if not os.path.isabs(path):
-	path = storage(path)
-
+        path = storage(path)
+    
     if not isinstance(path, six.binary_type):
-	with codecs.open(path, "r", "utf-8") as f:
-	    s = f.read()
-	return plistlib.readPlistFromString(s)
+        with codecs.open(path, "r", "utf-8") as f:
+            s = f.read()
+        return plistlib.readPlistFromString(s)
     else:
-	return biplist.readPlist(path)
+        return biplist.readPlist(path)
 
 
 def writePlist(obj, path):
     if not os.path.isabs(path):
-	path = storage(path)
-
+        path = storage(path)
+    
     s = plistlib.writePlistToString(obj)
     with codecs.open(path, "w", "utf-8") as f:
-	f.write(s)
+        f.write(s)
 
 
 def jsonLoad(path, default=None):
     if not os.path.isabs(path):
-	path = storage(path)
+        path = storage(path)
 
     if os.path.exists(path):
-	with codecs.open(path, "r", "utf-8") as f:
-	    read = json.load(f)
-	return read
+        with codecs.open(path, "r", "utf-8") as f:
+            read = json.load(f)
+        return read
     elif default != None:
-	with codecs.open(path, "w", "utf-8") as f:
-	    json.dump(default, f)
-	return default
+        with codecs.open(path, "w", "utf-8") as f:
+            json.dump(default, f)
+        return default
     else:
-	with codecs.open(path, "w", "utf-8") as f:
-	    f.write("\n")
-	return None
+        with codecs.open(path, "w", "utf-8") as f:
+            f.write("\n")
+        return None
 
 
 def jsonDump(obj, path):
     if not os.path.isabs(path):
-	path = storage(path)
+        path = storage(path)
 
     with codecs.open(path, "w", "utf-8") as f:
-	json.dump(obj, f)
+        json.dump(obj, f)
 
 
 def find(query):
@@ -134,14 +134,14 @@ def find(query):
     output = subprocess.check_output(qString, shell=True)
     returnList = output.split("\n")
     if returnList[-1] == "":
-	returnList = returnList[:-1]
+        returnList = returnList[:-1]
     return returnList
 
 
 def log(s):
     log_text = "[{0}: {1} ({2})]\n".format(bundle(), s, time.strftime("%Y-%m-%d-%H:%M:%S"))
     if not os.path.exists(local("debug.log")):
-	with open(local("debug.log"), "w") as f:
-	    f.write("\n")
+        with open(local("debug.log"), "w") as f:
+            f.write("\n")
     with codecs.open(local("debug.log"), "a", "utf-8") as f:
-	f.write(decode(log_text))
+        f.write(decode(log_text))
--- alp/core_dependencies/six.py
+++ alp/core_dependencies/six.py
@@ -47,22 +47,22 @@ else:
     binary_type = str
 
     if sys.platform.startswith("java"):
-	# Jython always uses 32 bits.
-	MAXSIZE = int((1 << 31) - 1)
+        # Jython always uses 32 bits.
+        MAXSIZE = int((1 << 31) - 1)
     else:
-	# It's possible to have sizeof(long) != sizeof(Py_ssize_t).
-	class X(object):
-	    def __len__(self):
-		return 1 << 31
-	try:
-	    len(X())
-	except OverflowError:
-	    # 32-bit
-	    MAXSIZE = int((1 << 31) - 1)
-	else:
-	    # 64-bit
-	    MAXSIZE = int((1 << 63) - 1)
-	    del X
+        # It's possible to have sizeof(long) != sizeof(Py_ssize_t).
+        class X(object):
+            def __len__(self):
+                return 1 << 31
+        try:
+            len(X())
+        except OverflowError:
+            # 32-bit
+            MAXSIZE = int((1 << 31) - 1)
+        else:
+            # 64-bit
+            MAXSIZE = int((1 << 63) - 1)
+            del X
 
 
 def _add_doc(func, doc):
@@ -79,54 +79,54 @@ def _import_module(name):
 class _LazyDescr(object):
 
     def __init__(self, name):
-	self.name = name
+        self.name = name
 
     def __get__(self, obj, tp):
-	result = self._resolve()
-	setattr(obj, self.name, result)
-	# This is a bit ugly, but it avoids running this again.
-	delattr(tp, self.name)
-	return result
+        result = self._resolve()
+        setattr(obj, self.name, result)
+        # This is a bit ugly, but it avoids running this again.
+        delattr(tp, self.name)
+        return result
 
 
 class MovedModule(_LazyDescr):
 
     def __init__(self, name, old, new=None):
-	super(MovedModule, self).__init__(name)
-	if PY3:
-	    if new is None:
-		new = name
-	    self.mod = new
-	else:
-	    self.mod = old
+        super(MovedModule, self).__init__(name)
+        if PY3:
+            if new is None:
+                new = name
+            self.mod = new
+        else:
+            self.mod = old
 
     def _resolve(self):
-	return _import_module(self.mod)
+        return _import_module(self.mod)
 
 
 class MovedAttribute(_LazyDescr):
 
     def __init__(self, name, old_mod, new_mod, old_attr=None, new_attr=None):
-	super(MovedAttribute, self).__init__(name)
-	if PY3:
-	    if new_mod is None:
-		new_mod = name
-	    self.mod = new_mod
-	    if new_attr is None:
-		if old_attr is None:
-		    new_attr = name
-		else:
-		    new_attr = old_attr
-	    self.attr = new_attr
-	else:
-	    self.mod = old_mod
-	    if old_attr is None:
-		old_attr = name
-	    self.attr = old_attr
+        super(MovedAttribute, self).__init__(name)
+        if PY3:
+            if new_mod is None:
+                new_mod = name
+            self.mod = new_mod
+            if new_attr is None:
+                if old_attr is None:
+                    new_attr = name
+                else:
+                    new_attr = old_attr
+            self.attr = new_attr
+        else:
+            self.mod = old_mod
+            if old_attr is None:
+                old_attr = name
+            self.attr = old_attr
 
     def _resolve(self):
-	module = _import_module(self.mod)
-	return getattr(module, self.attr)
+        module = _import_module(self.mod)
+        return getattr(module, self.attr)
 
 
 
@@ -173,14 +173,14 @@ _moved_attributes = [
     MovedModule("tkinter_constants", "Tkconstants", "tkinter.constants"),
     MovedModule("tkinter_dnd", "Tkdnd", "tkinter.dnd"),
     MovedModule("tkinter_colorchooser", "tkColorChooser",
-		"tkinter.colorchooser"),
+                "tkinter.colorchooser"),
     MovedModule("tkinter_commondialog", "tkCommonDialog",
-		"tkinter.commondialog"),
+                "tkinter.commondialog"),
     MovedModule("tkinter_tkfiledialog", "tkFileDialog", "tkinter.filedialog"),
     MovedModule("tkinter_font", "tkFont", "tkinter.font"),
     MovedModule("tkinter_messagebox", "tkMessageBox", "tkinter.messagebox"),
     MovedModule("tkinter_tksimpledialog", "tkSimpleDialog",
-		"tkinter.simpledialog"),
+                "tkinter.simpledialog"),
     MovedModule("urllib_robotparser", "robotparser", "urllib.robotparser"),
     MovedModule("winreg", "_winreg"),
 ]
@@ -199,12 +199,12 @@ def add_move(move):
 def remove_move(name):
     """Remove item from six.moves."""
     try:
-	delattr(_MovedItems, name)
+        delattr(_MovedItems, name)
     except AttributeError:
-	try:
-	    del moves.__dict__[name]
-	except KeyError:
-	    raise AttributeError("no such move, %r" % (name,))
+        try:
+            del moves.__dict__[name]
+        except KeyError:
+            raise AttributeError("no such move, %r" % (name,))
 
 
 if PY3:
@@ -239,7 +239,7 @@ try:
     advance_iterator = next
 except NameError:
     def advance_iterator(it):
-	return it.next()
+        return it.next()
 next = advance_iterator
 
 
@@ -247,31 +247,31 @@ try:
     callable = callable
 except NameError:
     def callable(obj):
-	return any("__call__" in klass.__dict__ for klass in type(obj).__mro__)
+        return any("__call__" in klass.__dict__ for klass in type(obj).__mro__)
 
 
 if PY3:
     def get_unbound_function(unbound):
-	return unbound
+        return unbound
 
     create_bound_method = types.MethodType
 
     Iterator = object
 else:
     def get_unbound_function(unbound):
-	return unbound.im_func
+        return unbound.im_func
 
     def create_bound_method(func, obj):
-	return types.MethodType(func, obj, obj.__class__)
+        return types.MethodType(func, obj, obj.__class__)
 
     class Iterator(object):
 
-	def next(self):
-	    return type(self).__next__(self)
+        def next(self):
+            return type(self).__next__(self)
 
     callable = callable
 _add_doc(get_unbound_function,
-	 """Get the function out of a possibly unbound function""")
+         """Get the function out of a possibly unbound function""")
 
 
 get_method_function = operator.attrgetter(_meth_func)
@@ -301,15 +301,15 @@ def iterlists(d, **kw):
 
 if PY3:
     def b(s):
-	return s.encode("latin-1")
+        return s.encode("latin-1")
     def u(s):
-	return s
+        return s
     if sys.version_info[1] <= 1:
-	def int2byte(i):
-	    return bytes((i,))
+        def int2byte(i):
+            return bytes((i,))
     else:
-	# This is about 2x faster than the implementation above on 3.2+
-	int2byte = operator.methodcaller("to_bytes", 1, "big")
+        # This is about 2x faster than the implementation above on 3.2+
+        int2byte = operator.methodcaller("to_bytes", 1, "big")
     indexbytes = operator.getitem
     iterbytes = iter
     import io
@@ -317,14 +317,14 @@ if PY3:
     BytesIO = io.BytesIO
 else:
     def b(s):
-	return s
+        return s
     def u(s):
-	return unicode(s, "unicode_escape")
+        return unicode(s, "unicode_escape")
     int2byte = chr
     def indexbytes(buf, i):
-	return ord(buf[i])
+        return ord(buf[i])
     def iterbytes(buf):
-	return (ord(byte) for byte in buf)
+        return (ord(byte) for byte in buf)
     import StringIO
     StringIO = BytesIO = StringIO.StringIO
 _add_doc(b, """Byte literal""")
@@ -337,9 +337,9 @@ if PY3:
 
 
     def reraise(tp, value, tb=None):
-	if value.__traceback__ is not tb:
-	    raise value.with_traceback(tb)
-	raise value
+        if value.__traceback__ is not tb:
+            raise value.with_traceback(tb)
+        raise value
 
 
     print_ = getattr(builtins, "print")
@@ -347,16 +347,16 @@ if PY3:
 
 else:
     def exec_(_code_, _globs_=None, _locs_=None):
-	"""Execute code in a namespace."""
-	if _globs_ is None:
-	    frame = sys._getframe(1)
-	    _globs_ = frame.f_globals
-	    if _locs_ is None:
-		_locs_ = frame.f_locals
-	    del frame
-	elif _locs_ is None:
-	    _locs_ = _globs_
-	exec("""exec _code_ in _globs_, _locs_""")
+        """Execute code in a namespace."""
+        if _globs_ is None:
+            frame = sys._getframe(1)
+            _globs_ = frame.f_globals
+            if _locs_ is None:
+                _locs_ = frame.f_locals
+            del frame
+        elif _locs_ is None:
+            _locs_ = _globs_
+        exec("""exec _code_ in _globs_, _locs_""")
 
 
     exec_("""def reraise(tp, value, tb=None):
@@ -365,49 +365,49 @@ else:
 
 
     def print_(*args, **kwargs):
-	"""The new-style print function."""
-	fp = kwargs.pop("file", sys.stdout)
-	if fp is None:
-	    return
-	def write(data):
-	    if not isinstance(data, basestring):
-		data = str(data)
-	    fp.write(data)
-	want_unicode = False
-	sep = kwargs.pop("sep", None)
-	if sep is not None:
-	    if isinstance(sep, unicode):
-		want_unicode = True
-	    elif not isinstance(sep, str):
-		raise TypeError("sep must be None or a string")
-	end = kwargs.pop("end", None)
-	if end is not None:
-	    if isinstance(end, unicode):
-		want_unicode = True
-	    elif not isinstance(end, str):
-		raise TypeError("end must be None or a string")
-	if kwargs:
-	    raise TypeError("invalid keyword arguments to print()")
-	if not want_unicode:
-	    for arg in args:
-		if isinstance(arg, unicode):
-		    want_unicode = True
-		    break
-	if want_unicode:
-	    newline = unicode("\n")
-	    space = unicode(" ")
-	else:
-	    newline = "\n"
-	    space = " "
-	if sep is None:
-	    sep = space
-	if end is None:
-	    end = newline
-	for i, arg in enumerate(args):
-	    if i:
-		write(sep)
-	    write(arg)
-	write(end)
+        """The new-style print function."""
+        fp = kwargs.pop("file", sys.stdout)
+        if fp is None:
+            return
+        def write(data):
+            if not isinstance(data, basestring):
+                data = str(data)
+            fp.write(data)
+        want_unicode = False
+        sep = kwargs.pop("sep", None)
+        if sep is not None:
+            if isinstance(sep, unicode):
+                want_unicode = True
+            elif not isinstance(sep, str):
+                raise TypeError("sep must be None or a string")
+        end = kwargs.pop("end", None)
+        if end is not None:
+            if isinstance(end, unicode):
+                want_unicode = True
+            elif not isinstance(end, str):
+                raise TypeError("end must be None or a string")
+        if kwargs:
+            raise TypeError("invalid keyword arguments to print()")
+        if not want_unicode:
+            for arg in args:
+                if isinstance(arg, unicode):
+                    want_unicode = True
+                    break
+        if want_unicode:
+            newline = unicode("\n")
+            space = unicode(" ")
+        else:
+            newline = "\n"
+            space = " "
+        if sep is None:
+            sep = space
+        if end is None:
+            end = newline
+        for i, arg in enumerate(args):
+            if i:
+                write(sep)
+            write(arg)
+        write(end)
 
 _add_doc(reraise, """Reraise an exception.""")
 
