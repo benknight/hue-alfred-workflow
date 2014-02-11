d9d82196b9f68b7aa8c1144925623041a9999fe9
Benjamin Knight <ben@benknight.me>
2014-02-10 23:41:15 -0800

    Whitespace ugh.. git was converting my spaces to tabs before

--- src/color_picker.py
+++ src/color_picker.py
@@ -4,11 +4,11 @@ import os
 class OSXColorPicker:
 
     @staticmethod
-    def color_picker(self, lid):
-	rgba = os.system(r"""osascript -e 'tell application "Alfred 2"' -e 'activate' -e 'choose color default color {65535, 65535, 65535}' -e 'end tell'""")
-	if not rgba:
-	    return false
-	rgba = rgba.split(',')
-	rgb = rgba[0:3]
-	hexColor = color.rgbToHex(rgb[0], rgb[1], rgb[2])
-	os.system(r"""osascript -e 'tell application "Alfred 2" to search "hue {0}:color:{1}"'""".format(lid, hexColor))
+    def color_picker(lid):
+        rgba = os.system(r"""osascript -e 'tell application "Alfred 2"' -e 'activate' -e 'choose color default color {65535, 65535, 65535}' -e 'end tell'""")
+        if not rgba:
+            return False
+        rgba = rgba.split(',')
+        rgb = rgba[0:3]
+        hexColor = color.rgbToHex(rgb[0], rgb[1], rgb[2])
+        os.system(r"""osascript -e 'tell application "Alfred 2" to search "hue {0}:color:{1}"'""".format(lid, hexColor))
