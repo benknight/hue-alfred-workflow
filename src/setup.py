3a9a9abc25ea1c66d784b079597294d03e853914
Benjamin Knight <ben@benknight.me>
2014-02-28 23:59:06 -0800

    Making groups management actually work in setup.py

--- src/setup.py
+++ src/setup.py
@@ -16,7 +16,7 @@ class HueAlfredSetup:
         else:
             bridge_ip = bridges[0]['internalipaddress']
             settings = alp.Settings()
-            settings.set(bridge_ip=bridge_ip, group=0)
+            settings.set(bridge_ip=bridge_ip, group='/groups/0')
 
             # Create API user for the workflow
             r = requests.post(
@@ -37,11 +37,11 @@ class HueAlfredSetup:
         settings = alp.Settings()
 
         if arg == '0':
-            settings.set(group=0)
+            settings.set(group='')
         else:
             lights = arg.split(',')
 
-            if not settings.get('custom_group'):
+            if not settings.get('group_id'):
                 # Create a custom group for the workflow to use
                 r = requests.post(
                     'http://{bridge_ip}/api/{user}/groups'.format(
@@ -51,17 +51,29 @@ class HueAlfredSetup:
                     data=json.dumps({
                         'name': 'Afred Hue Group',
                         'lights': lights,
-                    }))
+                    })
+                )
+
+                resp = r.json()
+
+                if len(resp) > 0:
+                    if resp[0].get('error'):
+                        print 'Setup Error: %s' % resp[0]['error'].get('description')
+                    else:
+                        settings.set(group_id=resp[0]['success']['id'])
             else:
                 r = requests.put(
-                    'http://{bridge_ip}/api/{user}/groups/{group_id}'.format(
+                    'http://{bridge_ip}/api/{user}{group_id}'.format(
                         bridge_ip=settings.get('bridge_ip'),
                         user=settings.get('username'),
-                        group_id=settings.get('custom_group'),
+                        group_id=settings.get('group_id'),
                     ),
                     data=json.dumps({
                         'lights': lights,
-                    }))
+                    })
+                )
+
+            settings.set(group=lights)
 
         return None
 
