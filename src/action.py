ad89dedaca47cdb56aa2944913885da49a6d1772
Benjamin Knight <ben@benknight.me>
2014-03-04 20:52:50 -0800

    Refactoring action.py to use hue_request

--- src/action.py
+++ src/action.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import datetime
 import json
 import os
 import re
@@ -10,6 +11,7 @@ import requests
 
 import css_colors
 import rgb_cie
+from hue_request import HueRequest
 
 
 class HueAlfredAction:
@@ -17,11 +19,7 @@ class HueAlfredAction:
     def __init__(self):
         self.settings = alp.Settings()
         self.group_id = self.settings.get('group_id') if self.settings.get('group') else '/groups/0'
-        self.request_path = '/api/%s' % self.settings.get('username')
-        self.request_path_full = 'http://{bridge_ip}{request_path}'.format(
-            bridge_ip=self.settings.get('bridge_ip'),
-            request_path=self.request_path,
-        )
+        self.hue_request = HueRequest()
 
     def _get_xy_color(self, color):
         """Validate and convert hex color to XY space."""
@@ -41,28 +39,14 @@ class HueAlfredAction:
 
         return converter.hexToCIE1931(color)
 
-    def _save_current_state_as_preset(self, name):
-        lights = alp.jsonLoad(alp.cache('lights.json'))
-
-        # Create dir
-        preset_dir = alp.storage(join='presets/' + name)
-        os.makedirs(preset_dir)
-
-        # Dump lights.json
-        alp.jsonDump(lights, preset_dir + '/lights.json')
-
-        # for each light do this
-        for lid in lights:
-            light = alp.jsonLoad(alp.cache('%s.json' % lid))
-            alp.jsonDump(light, preset_dir + ('/%s.json' % lid))
-
     def _load_preset(self, preset_name):
         lights = alp.jsonLoad('presets/%s/lights.json' % preset_name)
 
         for lid in lights:
             light = alp.jsonLoad('presets/{0}/{1}.json'.format(preset_name, lid))
-            requests.put(
-                self.request_path_full + ('/lights/%s/state' % lid),
+            self.hue_request.request(
+                'put',
+                '/lights/%s/state' % lid,
                 json.dumps({
                      'xy': light['state']['xy'],
                      'on': light['state']['on'],
@@ -72,10 +56,13 @@ class HueAlfredAction:
 
     def send_filter_query(self, json_query):
         """Decodes a json query sent by the script filter and makes the API call.
-        This whole system is rather inelegant, with complicated, hard-to-understand logic.
-        Right now I'm too lazy to come up with a better way, so deal with it. :D
+        This whole system is rather inelegant, but right now I'm too lazy to come
+        up with a better way, so deal with it. :D
         """
-        query = json.loads(json_query)
+        if json_query.startswith('colorpicker'):
+            return None
+        else:
+            query = json.loads(json_query)
 
         if query.get('action') == 'load_preset':
             self._load_preset(query['preset_name'])
@@ -84,15 +71,25 @@ class HueAlfredAction:
             self._save_current_state_as_preset(query['preset_name'])
 
         elif query.get('action') == 'rename':
-            endoint = '/lights/%s' % query['lid']
-            data = json.dumps(query['data'])
-            requests.put(self.request_path_full + endpoint, data)
+            self.hue_request.request(
+                'put',
+                '/lights/%s' % query['lid'],
+                json.dumps(query['data']),
+            )
 
         elif query.get('action') == 'reminder':
             if query['lid'] == 'all':
-                address = self.request_path + ('%s/action' % self.group_id)
+                address = self.hue_request.api_path + ('%s/action' % self.group_id)
             else:
-                address = self.request_path + ('/lights/%s/state' % query['lid'])
+                address = self.hue_request.api_path + ('/lights/%s/state' % query['lid'])
+
+            try:
+                time_delta_int = int(query['time_delta'])
+            except ValueError:
+                print 'Invalid time delta for reminder.'
+                return None
+
+            reminder_time = datetime.datetime.utcfromtimestamp(time.time() + time_delta_int)
 
             data = json.dumps({
                 'name': 'Alfred Hue Reminder',
@@ -101,29 +98,26 @@ class HueAlfredAction:
                     'method': 'PUT',
                     'body': {'alert': 'lselect'},
                 },
-                'time': time.strftime(
-                    '%Y-%m-%dT%H-%M-%S',
-                    time.gmtime(time.time() + int(query['time_delta']))
-                ),
+                'time': reminder_time.replace(microsecond=0).isoformat(),
             })
-            requests.post(self.request_path_full + '/schedules', data)
+            self.hue_request.request('post', '/schedules', data)
 
         else:
             if query.get('action') == 'set_color':
                 try:
-                    data = json.dumps({'xy': self._get_xy_color(query['color'])})
+                    data = {'xy': self._get_xy_color(query['color'])}
                 except ValueError:
                     print 'Invalid color. Please use a 6-digit hex color.'
                     return None
             else:
-                data = json.dumps(query['data'])
+                data = query['data']
 
             if query['lid'] == 'all':
                 endpoint = '%s/action' % self.group_id
             else:
                 endpoint = '/lights/%s/state' % query['lid']
 
-            requests.put(self.request_path_full + endpoint, data)
+            self.hue_request.request('put', endpoint, json.dumps(data))
 
 
 if __name__ == '__main__':
