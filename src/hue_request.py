1ac1057177b1492e0ccb3e4698fe61623d5a204c
Benjamin Knight <ben@benknight.me>
2014-03-04 20:52:10 -0800

    Request abstraction for logging

---
+++ src/hue_request.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import alp
+import requests
+
+
+class HueRequest():
+    """Handles Philips Hue API path strings and logs every request for debugging."""
+
+    def __init__(self):
+        self.settings = alp.Settings()
+        self.api_path = '/api/%s' % self.settings.get('username')
+        self.api_path_full = 'http://{bridge_ip}{api_path}'.format(
+            bridge_ip=self.settings.get('bridge_ip'),
+            api_path=self.api_path,
+        )
+
+    def request(self, method, endpoint, data=None):
+        requests.request(method, self.api_path_full + endpoint, data=data)
+        alp.log('request({method}, {endpoint}, {data})'.format(
+            method=method, endpoint=endpoint, data=data,
+        ))
