e4af011b4b95783c9fd49ede6b439a6ec9a6f728
Benjamin Knight <ben@benknight.me>
2015-02-01 02:00:58 -0800

    Files that should have been in the last commit

---
+++ src/api.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+import datetime
+import json
+import os
+import random
+import re
+import sys
+import time
+
+from .packages import alp
+
+from . import colors
+from . import request
+from . import utils
+
+
+class HueAPI:
+
+    def __init__(self):
+        self.settings = alp.Settings()
+        self.group_id = self.settings.get('group_id') if self.settings.get('group') else '/groups/0'
+        self.hue_request = request.HueRequest()
+
+    def _get_xy_color(self, color):
+        """Validate and convert hex color to XY space."""
+        converter = colors.Converter()
+        hex_color_re = re.compile(
+            r'(?<!\w)([a-f0-9]){2}([a-f0-9]){2}([a-f0-9]){2}\b',
+            re.IGNORECASE
+        )
+
+        if color in colors.CSS_LITERALS:
+            color = colors.CSS_LITERALS[color]
+
+        color = color.lstrip('#')
+
+        if not hex_color_re.match(color):
+            raise ValueError()
+
+        return converter.hexToCIE1931(color)
+
+    def _load_preset(self, preset_name):
+        lights = alp.jsonLoad('presets/%s/lights.json' % preset_name)
+
+        for lid, light_data in lights.iteritems():
+            self.hue_request.request(
+                'put',
+                '/lights/%s/state' % lid,
+                json.dumps({
+                     'xy': light_data['state']['xy'],
+                     'on': light_data['state']['on'],
+                    'bri': light_data['state']['bri'],
+                }),
+            )
+
+    def execute(self, action):
+        control = action.split(':')
+
+        if control[0] == 'lights':
+            lid = control[1]
+            function = control[2]
+            value = control[3] if len(control) > 3 else None
+
+            # Default API request parameters
+            method = 'put'
+            endpoint = '%s/action' % self.group_id if lid == 'all' else '/lights/%s/state' % lid
+
+            if function == 'off':
+                data = {'on': False}
+
+            if function == 'on':
+                data = {'on': True}
+
+            if function == 'bri':
+                data = {'bri': value}
+
+            if function == 'rename':
+                endpoint = '/lights/%s' % lid
+                data = {'name': value}
+
+            if function == 'effect':
+                data = {'effect': value}
+
+            if function == 'color':
+                try:
+                    data = {'xy': self._get_xy_color(value)}
+                except ValueError:
+                    print 'Invalid color. Please use a 6-digit hex color.'
+                    return None
+
+            if function == 'reminder':
+                try:
+                    time_delta_int = int(value)
+                except ValueError:
+                    print 'Invalid time delta for reminder.'
+                    return None
+
+                if lid == 'all':
+                    address = self.hue_request.api_path + ('%s/action' % self.group_id)
+                else:
+                    address = self.hue_request.api_path + ('/lights/%s/state' % lid)
+
+                reminder_time = datetime.datetime.utcfromtimestamp(time.time() + time_delta_int)
+
+                data = json.dumps({
+                    'name': 'Alfred Hue Reminder',
+                    'command': {
+                        'address': address,
+                        'method': 'PUT',
+                        'body': {'alert': 'lselect'},
+                    },
+                    'time': reminder_time.replace(microsecond=0).isoformat(),
+                })
+
+            # Make the request
+            self.hue_request.request(method, endpoint, json.dumps(data))
+            print 'Action completed: %s' % action
+
+        if control[0] == 'presets':
+            # Load or save
+            action = control[1]
+            preset_name = control[2]
+
+            if action == 'load':
+                self._load_preset(preset_name)
+
+        if control[0] == 'random':
+            lights = utils.get_lights()
+
+            if not lights:
+                print 'No Hue lights found. Try -hue set-bridge.'
+                return None
+
+            for lid in lights:
+                self.hue_request.request(
+                    'put',
+                    '/lights/%s/state' % lid,
+                    json.dumps({
+                        'hue': random.randrange(0, 65535),
+                        'sat': 255,
+                    })
+                )
+            print 'Lights set to random hues.'
+
+
+
+if __name__ == '__main__':
+    hue_action = HueAlfredAction()
+    hue_action.send_filter_query(sys.argv[0])
---
+++ src/setup.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+import json
+
+from .packages import alp
+from .packages import requests
+
+
+def setup(bridge_ip=None):
+    if not bridge_ip:
+        r = requests.get('http://www.meethue.com/api/nupnp')
+        bridges = r.json()
+
+        if not bridges:
+            print 'No bridges found on your network.  Try specifying the IP if you know it.'
+        else:
+            bridge_ip = bridges[0]['internalipaddress']
+
+    if bridge_ip:
+        settings = alp.Settings()
+
+        # Create API user for the workflow
+        r = requests.post(
+            'http://{bridge_ip}/api'.format(bridge_ip=bridge_ip),
+            data=json.dumps({'devicetype': 'Alfred 2'}))
+
+        resp = r.json()[0]
+
+        if resp.get('error'):
+            print 'Setup Error: %s' % resp['error'].get('description')
+        else:
+            settings.set(bridge_ip=bridge_ip, group='')
+            settings.set(username=resp['success']['username'])
+            print 'Success! You can now control your lights by using the "hue" keyword.'
+
+
+def set_group(group):
+    settings = alp.Settings()
+
+    if group == '0':
+        settings.set(group='')
+    else:
+        lights = group.split(',')
+
+        if not settings.get('group_id'):
+            # Create a custom group for the workflow to use
+            r = requests.post(
+                'http://{bridge_ip}/api/{user}/groups'.format(
+                    bridge_ip=settings.get('bridge_ip'),
+                    user=settings.get('username')
+                ),
+                data=json.dumps({
+                    'name': 'Afred Hue Group',
+                    'lights': lights,
+                })
+            )
+
+            resp = r.json()
+
+            if len(resp) > 0:
+                if resp[0].get('error'):
+                    print 'Setup Error: %s' % resp[0]['error'].get('description')
+                else:
+                    settings.set(group_id=resp[0]['success']['id'])
+        else:
+            r = requests.put(
+                'http://{bridge_ip}/api/{user}{group_id}'.format(
+                    bridge_ip=settings.get('bridge_ip'),
+                    user=settings.get('username'),
+                    group_id=settings.get('group_id'),
+                ),
+                data=json.dumps({
+                    'lights': lights,
+                })
+            )
+
+        settings.set(group=lights)
