benknight/hue-alfred-workflow

commit a11b346260adf24922b7587f5a1fe6f15de0973e

Handle multiple queries at once
The documentation says that this action can handle multiple queries passed into it when separated with a `|`, but it looks like that wasn't actually implemented anywhere. This enables that behavior.

logic/action.py
@@ -200,20 +200,22 @@ class HueAction:
 
         return
 
-
 def main(workflow):
-    query = workflow.args[0].split(':')
-
-    if query[0] == 'set_bridge':
-        bridge_ip = workflow.args[0].split(':', 1)[1]
-        setup.set_bridge(bridge_ip)
-    else:
-        action = HueAction()
-        try:
-            action.execute(query)
-            print(('Action completed! <%s>' % workflow.args[0]).encode('utf-8'))
-        except ValueError:
-            pass
+    # Handle multiple queries separated with '|' (pipe) character
+    queries = workflow.args[0].split('|')
+    
+    for this_query in queries:
+        query = this_query.split(':')
+        if this_query == 'set_bridge':
+            bridge_ip = this_query.split(':', 1)[1]
+            setup.set_bridge(bridge_ip)
+        else:
+            action = HueAction()
+            try:
+                action.execute(query)
+                print(('Action completed! <%s>' % this_query).encode('utf-8'))
+            except ValueError:
+                pass
 
 
 if __name__ == '__main__':

commit 85f0b9a2b2c1532c9516fa6f300c3581d1066d7b

Fix action splitting
Bug introduced in #57

logic/action.py
@@ -204,16 +204,15 @@ def main(workflow):
     # Handle multiple queries separated with '|' (pipe) character
     queries = workflow.args[0].split('|')
     
-    for this_query in queries:
-        query = this_query.split(':')
-        if this_query == 'set_bridge':
-            bridge_ip = this_query.split(':', 1)[1]
-            setup.set_bridge(bridge_ip)
+    for query_str in queries:
+        query = query_str.split(':')
+        if query[0] == 'set_bridge':
+            setup.set_bridge(query[1] if len(query) > 1 else None)
         else:
             action = HueAction()
             try:
                 action.execute(query)
-                print(('Action completed! <%s>' % this_query).encode('utf-8'))
+                print(('Action completed! <%s>' % query_str).encode('utf-8'))
             except ValueError:
                 pass
 